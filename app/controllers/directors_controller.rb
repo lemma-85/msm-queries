2e92636becf95637a6f3346c2ea482d21a80eee3
lemma-85 <rmamuye@chicagobooth.edu>
2025-05-01 16:21:50 +0000

    final push

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -14,4 +14,15 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/details"})
   end 
 
+  def youngest 
+    @the_youngest_director = Director.where.not(dob: nil).order(dob: :desc).first
+
+    render({ :template => "director_templates/youngest"})
+  end 
+  
+  def eldest 
+    @the_eldest_director = Director.where.not(dob: nil).order(dob: :asc).first
+    
+    render({ :template => "director_templates/eldest"})
+  end 
 end
